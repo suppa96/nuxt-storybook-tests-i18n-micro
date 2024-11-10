df31a5e01e63b216103ccb83978dbedacd22a0a1
Martin Masevski <martin.masevski@gmail.com>
2024-11-10 17:14:30 +0100

    feat: enhance HelloWorld component with form submission and success message

--- app/components/HelloWorld.stories.ts
+++ app/components/HelloWorld.stories.ts
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
+import { userEvent, within, expect } from '@storybook/test'
+
 import HelloWorld from './HelloWorld.vue'
 import type { HelloWorldProps } from './HelloWorld.props'
 
@@ -24,5 +26,23 @@ export const Default: Story = {
   render: (args) => Template(args),
   args: {
     msg: 'Hello World',
+  },
+}
+
+export const Submitted: Story = {
+  ...Default,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    // 👇 Simulate interactions with the component
+    await userEvent.type(canvas.getByTestId('email'), 'email@provider.com')
+
+    await userEvent.type(canvas.getByTestId('password'), 'a-random-password')
+
+    // See https://storybook.js.org/docs/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
+    await userEvent.click(canvas.getByRole('button'))
+
+    // 👇 Assert DOM structure
+    await expect(canvas.getByText('Everything is perfect. Your account is ready and we should probably get you started!')).toBeInTheDocument()
   },
 }
