27aa86e103c8a26ce4a62a41d036e5a6758215f7
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-01-23 13:02:17 +0100

    chore: remove nust storyboook from app config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   future: {
     compatibilityVersion: 4,
   },
   devtools: { enabled: true },
-  modules: ['@nuxtjs/storybook', '@nuxt/icon'],
-  icon: {
-    customCollections: [
-      {
-        prefix: 'icon',
-        dir: './app/assets/icons',
-      },
-    ],
-  },
-})
+});
